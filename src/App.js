21294fa9b522a258ee1837806dd625d7d9090cb0
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-05 20:30:48 +0900

    PAGESコンポーネントの実装

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaltLayout } from "./components/templates/DefaltLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  const user = {
-    image: "https://source.unsplash.com/NE0XGVKTmcA",
-    name: "john",
-    email: "12345@example.com",
-    phone: "000-0000-0000",
-    company: {
-      name: "test株式会社"
-    },
-    website: "https://google.com"
-  };
-  return (
-    <BrowserRouter>
-      <DefaltLayout>
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaltLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
